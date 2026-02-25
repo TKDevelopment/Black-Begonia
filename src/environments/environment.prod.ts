d286befd9bae1bdb1359bd1e401bc6e92e0e9bd9
QuintsGuy <knotttristan@gmail.com>
2026-02-25 10:37:10 -0500

    Updating configurations for deployments

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,6 @@
 export const environment = {
     production: true,
     bypassAuth: false,
-    baseUrl: 'https://blackbegoniaflorals.com',
     supabaseUrl: 'default',
     supabaseKey: 'default',
     googleClientId: 'default',
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,6 @@
 export const environment = {
     production: 'default',
     bypassAuth: 'default',
-    baseUrl: 'default',
     supabaseUrl: 'default',
     supabaseKey: 'default',
     googleClientId: 'default',
